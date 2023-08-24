1653046fe2bfc2dbadd0417bfcc27500bef4ea2f
prasadbylapudi <myemail>
2023-08-24 09:59:56 +0530

    initial commit

---
+++ schema.js
@@ -0,0 +1,52 @@
+export const typeDefs = `#graphql
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
+  }
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+    author: Author!
+    game: Game!
+  }
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]
+  }
+  type Query {
+    games: [Game]
+    game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
+    authors: [Author]
+    author(id: ID!): Author
+  }
+
+  type Mutation {
+    addGame(game: AddGameInput!) : Game
+    updateGame(id : ID, edits:EditGameInput!): Game
+    # addReview(rating: Int!, content: String!, author_id: ID!, game_id: ID!): Review
+    # addAuthor(name: String!, verified: Boolean!): Author
+    deleteGame(id: ID!): [Game]
+
+    # deleteReview(id: ID!): Review
+    # deleteAuthor(id: ID!): Author
+
+  }
+
+  input AddGameInput{
+    title: String!
+    platform:[String!]!
+  }
+
+    input EditGameInput{
+        title: String
+        platform:[String!]
+    }
+
+`
